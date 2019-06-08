5074ab5e543194a7d87eae1ec00ff440a1966cdf
Stephanie Mattern <smattern416@gmail.com>
2019-06-08 13:04:10 -0400

    got questions to switch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,118 +1,89 @@
 $(document).ready(function(){
+    // variables
 
-// variables
+    var count = 0;
+    var time = 31;
+    var selection = false;
+    var clock;
+    var right = 0;
+    var wrong = 0;
+    var noAnswer = 0;
 
-var time = 30;
-var count = 0;
-var time;
-var selection = false;
-var right = 0;
-var wrong = 0;
-var unanswered = 0;
-
- // question: a string
 var question = ["The average person does what 13 times a day?", "The Average American does what 22 times a day?", "In California you can't legally buy a mousetrap without having a what?", "Coprastastaphobia is the fear of what?", "The first puck used In the game of ice hockey game, what was the first puck used made out of?"];
  
-// Right answers:
+// // right answers:
 var answer =  ["Laughs", "Opens fridge", "Hunting License", "Constipation", "Frozen Cow Manure"];  
 
-// multiple choice options:
+// // multiple choice options:
 var optionOne =  ["Smiles", "Texts", "Hunting License", "Public Speaking", "Can of Beans"];  
 var optionTwo =  ["Laughs", "Drinks Water", "Drivers License", "Outer Space", "Frozen Cow Manure"];  
 var optionThree =  ["Cries", "Turns on faucet", "Pest Control License", "Constipation", "Set of Dentures"];  
-var optionFour =  ["Yawns", "Opens fridge", "Fishing License", "Cars", "A Carved Piece of Wood"];  
+var optionFour =  ["Yawns", "Opens fridge", "Fishing License", "Cars", "A Carved Piece of Wood"]; 
 
-// create click events
-//  functions
-function showHolders() {
-    $("#question-holder").show();
-    $("#choice1-holder").show();
-    $("#choice2-holder").show();
-    $("#choice3-holder").show();
-    $("#choice4-holder").show();
-}
-
-function hideHolders() {
-    $("#question-holder").hide();
-    $("#choice1-holder").hide();
-    $("#choice2-holder").hide();
-    $("#choice3-holder").hide();
-    $("#choice4-holder").hide();
-}
-function hideResults() {
-    $("#Right-holder").hide();
-    $("#wrong-holder").hide();
-    $("#noAnswer-holder").hide();
-    $("#restart-holder").hide();
-}
-function displayQuestion () {
-    hideResults();
-    $("#answer-holder").hide();
-    $("#time-holder").show();
-    showHolders();
-    $("#question-holder").html(question[count]);
-    $("#choice1-holder").html(optionOne[count]);
-    $("#choice2-holder").html(optionTwo[count]);
-    $("#choice3-holder").html(optionThree[count]);
-    $("#choice4-holder").html(optionFour[count]);
+// Show & Hide Functions
+    function showHolders() {
+        $("#question-holder").show();
+        $("#choice1-holder").show();
+        $("#choice2-holder").show();
+        $("#choice3-holder").show();
+        $("#choice4-holder").show();
+    }
+    function hideHolders() {
+        $("#question-holder").hide();
+        $("#choice1-holder").hide();
+        $("#choice2-holder").hide();
+        $("#choice3-holder").hide();
+        $("#choice4-holder").hide();
+    }
+    function hideResults() {
+        $("#right-holder").hide();
+        $("#wrong-holder").hide();
+        $("#noAnswer-holder").hide();
+        $("#restart-holder").hide();
+    }
+    function displayQuestion () {
+        hideResults();
+        $("#answer-holder").hide();
+        $("#time-holder").show();
+        showHolders();
+        $("#question-holder").html(question[count]);
+        $("#choice1-holder").html(optionOne[count]);
+        $("#choice2-holder").html(optionTwo[count]);
+        $("#choice3-holder").html(optionThree[count]);
+        $("#choice4-holder").html(optionFour[count]);
     
-    
-    $("#choice1-holder").hover(function() {
-        $(this).css("color", "gray");
-    },
-    function(){
-        $(this).css("color", "black");
-    });
-    $("#choice2-holder").hover(function() {
-        $(this).css("color", "gray");
-    },
-    function(){
-        $(this).css("color", "black");
-    });
-    $("#choice3-holder").hover(function() {
-        $(this).css("color", "gray");
-    },
-    function(){
-        $(this).css("color", "black");
-    });
-    $("#choice4-holder").hover(function() {
-        $(this).css("color", "gray");
-    },
-    function(){
-        $(this).css("color", "black");
-    });
-}
-
-$("#choice1-holder").on("click", checkAnswer) 
+    }
+    $("#choice1-holder").on("click", checkAnswer) 
     $("#choice2-holder").on("click", checkAnswer)
     $("#choice3-holder").on("click", checkAnswer)
     $("#choice4-holder").on("click", checkAnswer)
 
-function checkAnswer() {
-    hideHolders();
+// Check the answer
+    function checkAnswer() {
+
+        hideHolders();
 
         if($(this).text() === answer[count]) {
             stopTime();
-            isSelected = true;
+            selection = true;
             $("#answer-holder").show();
-            $("#answer-holder").html("That's right! The answer is: " + answer[count]);
-            displayImage();
+            $("#answer-holder").html("Right! The answer is: " + answer[count]);
             right++;
             count++;
-
         }
         else {
             stopTime();
-            isSelected = true;
+            selection = true;
             $("#answer-holder").show();
             $("#answer-holder").html("Wrong! The answer is: " + answer[count]);
-            displayImage();
-            right++;
+            wrong++;
             count++;
-        }
-        checkGameEnd();
+        } 
+
+        checkGameEnd();  
     }
 
+// End of the Game 
     function checkGameEnd() {
         if(count === question.length) {
             $("#time-holder").hide();
@@ -122,30 +93,29 @@ function checkAnswer() {
             $(".start").on("click", function() {
                 resetResults();
                 startGame();
-
             });
         }
     }
+
     function resetTime() {
         time = 31;
     }
-    
+
     function displayTime() {
         time--;
         $("#time-holder").html("Time remaining: " + time);
-
-
-        if(time <= 0) {
-            hideHolders();
-            stopTime();
-            $("#answer-holder").show();
-            $("#answer-holder").html("Your time is up! The answer is: " + answer[count]);
-            displayImage();
-            noAnswer++;
-            count++;
-            checkGameEnd();
-        }
+      
+            if(time <= 0) {
+                hideHolders();
+                stopTime();
+                $("#answer-holder").show();
+                $("#answer-holder").html("Time is up! The answer is: " + answer[count]);
+                noAnswer++;
+                count++;
+                checkGameEnd();
+            }
     }
+
     function startTime() {
         clearInterval(clock);
         clock = setInterval(displayTime, 1000);
@@ -155,38 +125,40 @@ function checkAnswer() {
         resetTime();
         if(count < question.length - 1) {
             setTimeout(startTime, 2000);
-            setTimeout = (displayQuestion, 3000);  
+            setTimeout(displayQuestion, 3000);
         }
-        
     }
 
     resetTime();
 
+    // Show Results    
     function showResults() {
         $("#right-holder").show();
-        $("#right-holder").html("right: " + Right);
+        $("#right-holder").html("right: " + right);
         $("#wrong-holder").show();
-        $("#wrong-holder").html("Wrong: " + wrong);
+        $("#wrong-holder").html("wrong: " + wrong);
         $("#noAnswer-holder").show();
-        $("#noAnswer-holder").html("No answer: " + noAnswer);
+        $("#noAnswer-holder").html("noAnswer: " + noAnswer);
         $("#restart-holder").show();
         $("#restart-holder").html("Click Start to play again!");
     }
-// resetting the game
+
+// Reset Results  
     function resetResults() {
-        Right = 0;
+        correct = 0;
         wrong = 0;
         noAnswer = 0;
     }
 
+// Start  Function
     function startGame() {
         $(".start").hide();
-        startTime;
+        startTime();
         displayQuestion();
     }
-    // Start the game with clicking the start button
-    $(".start").on("click", function(){
-        startGame();
-    });
 
+// Start on click
+  $(".start").on("click", function() {
+    startGame();
+  });
 });
